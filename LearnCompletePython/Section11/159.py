e3c19d5af181345d14c08c2fa43268a010681fd8
Bernardo Paulsen <bernardohpaulsen@gmail.com>
2021-02-01 18:57:15 -0300

    159

--- LearnCompletePython/Section11/159.py
+++ LearnCompletePython/Section11/159.py
@@ -4,3 +4,15 @@ Bernardo Paulsen
 Exercise from class 159 from section 11
 from Learn Complete Python In Simple Way.
 """
+
+# REVERSE method
+
+l = [10,20,30,40]
+print(l)
+l.reverse()
+print(l)
+
+l = [10,20,30,40]
+print(l)
+print(reversed(l))
+print(list(reversed(l)))
---
+++ LearnCompletePython/Section11/160.py
@@ -0,0 +1,6 @@
+"""
+Bernardo Paulsen
+
+Exercise from class 160 from section 11
+from Learn Complete Python In Simple Way.
+"""
