4c6a82b18d66ab38decdab9ccfd7847330eb4691
bernardopaulsen <bernardopaulsen@gmail.com>
2021-02-01 17:41:35 +0000

    exercise 155

--- LearnCompletePython/Section11/155.py
+++ LearnCompletePython/Section11/155.py
@@ -4,3 +4,19 @@ Bernardo Paulsen
 Exercise from class 155 from section 11
 from Learn Complete Python In Simple Way.
 """
+
+# APPEND function
+l = []
+for i in range(10):
+    l.append(i)
+print(l)
+
+# INSERT function
+for i in range(10):
+    l.insert(i*2,i)
+print(l)
+
+l = []
+for i in range(10):
+    l.insert(0,i)
+print(l)
---
+++ LearnCompletePython/Section11/156.py
@@ -0,0 +1,6 @@
+"""
+Bernardo Paulsen
+
+Exercise from class 156 from section 11
+from Learn Complete Python In Simple Way.
+"""
