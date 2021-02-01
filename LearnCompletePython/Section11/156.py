8bb27b404a724293e9ba5a9a10a8c2e94f9850be
bernardopaulsen <bernardopaulsen@gmail.com>
2021-02-01 21:15:49 +0000

    156,57

--- LearnCompletePython/Section11/156.py
+++ LearnCompletePython/Section11/156.py
@@ -4,3 +4,13 @@ Bernardo Paulsen
 Exercise from class 156 from section 11
 from Learn Complete Python In Simple Way.
 """
+
+# EXTHEND method
+l1 = [1,2,3]
+l2 = [4,5,6]
+l1.extend(l2)
+print(l1)
+
+l1 = [10,20,30]
+l1.extend("ABC")
+print(l1)
---
+++ LearnCompletePython/Section11/157.py
@@ -0,0 +1,8 @@
+"""
+Bernardo Paulsen
+
+Exercise from class 157 from section 11
+from Learn Complete Python In Simple Way.
+"""
+
+# REMOVE method
